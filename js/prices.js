208ea12b3aa5dce1a0a1601930dca165e7257eb1
RyanSchmaldinst <36047456+RyanSchmaldinst@users.noreply.github.com>
2018-07-09 16:05:43 -0400

    JQuery

--- js/prices.js
+++ js/prices.js
@@ -1,19 +1,21 @@
 var totalValue = 0;
 
 $(document).on("click",".pickButton", function () {
+  var thisValue = $(this).attr('value');
+  var thisConvertedValue = parseInt(thisValue);
 
   $(this).toggleClass('transform-active');
 
   if ($(this).attr('clicked') == 'false')
   {
-    $(this).attr('clicked', 'true')
-    var thisValue = $(this).attr('value');
-    var thisConvertedValue = parseInt(thisValue);
+    $(this).attr('clicked', 'true');
     totalValue += thisConvertedValue;
     console.log(totalValue);
   }
   else
   {
+    totalValue -= thisConvertedValue
+    $(this).attr('clicked', 'false');
     console.log($(this) + ' ' + $(this).attr('clicked'));
   }
 });
